3c6a156099ab417a3a434b709cc1bcf45991f0c9
Andrey_B <example@gmail.com>
2021-11-16 15:34:19 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import './styles/app.scss'
+import Home from './pages/home/Home';
+import Watch from './pages/watch/Watch';
+import Registrate from './pages/register/Registrate';
+import Login from './pages/login/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from 'react';
+import {AuthContext} from './authContext/AuthContext'
+
 const App = () => {
-  return <div>Hello world!</div>;
+	const {user} = useContext(AuthContext)
+  return (
+    <Router>
+			<Switch>
+				<Route exact path="/">
+						{user ? <Home/> : <Redirect to="/login"/>}
+				</Route>
+				<Route exact path="/register">
+						{!user ? <Registrate/> : <Redirect to="/"/>}
+				</Route>
+				<Route exact path="/login">
+						{!user ? <Login/> : <Redirect to="/"/>}
+				</Route>
+				{user && (
+					<>
+						<Route exact path="/movies">
+							<Home type="movie"/>	
+						</Route>
+						<Route exact path="/series">
+							<Home type="series"/>
+						</Route>
+						<Route exact path="/watch">
+							<Watch/>
+						</Route>
+					</>
+				)}
+				<Redirect to="/"/>
+			</Switch>
+    </Router>
+  );
 };
 
 export default App;
